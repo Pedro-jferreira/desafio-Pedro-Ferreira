438faffcd5c4d7c332b7dd505c71711c28b0cca7
Pedro Ferreira <pedrojferreiradev@gmail.com>
2023-08-13 08:59:46 -0300

    update class  CaixaDaLanchonete

--- src/cashier/caixa-da-lanchonete.js
+++ src/cashier/caixa-da-lanchonete.js
@@ -1,4 +1,11 @@
+import { Cardapio } from "../menu/Cardapio";
+import { Pagamento } from "../payment/Pagamento";
+
 class CaixaDaLanchonete {
+    constructor(){
+        this.Cardapio = new Cardapio;
+        this.Pagamento = new Pagamento
+    }
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         return "";
